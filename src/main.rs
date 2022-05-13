dc17b97d5aa92fcbb8ee9df47d666ae4e269a334
Omer Faruk Dogan <omer.crlys.44@gmail.com>
2022-05-13 22:56:58 +0300

    First Commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
-use std::mem;
-
-fn main() {
-aritmetikIslemler()
+fn if_state(){
+    let sicaklik=25;
+    if sicaklik>20 {
+        println!("Sıcaklık 20 den büyüktür.")
+    }
+    let gun=if sicaklik>20 {"Günesli"} else { "Bulutlu" };
+    println!("Gün : {} ",gun);
 }
-fn aritmetikIslemler(){
-    let a=2;
-    let b=257;
-    //let a_kupu=i32::pow(a,3);
-    println!("{} nin 3. kuvveti= {}",a,i32::pow(a,3));
-println!("{} nın {} ile bölümünden kalan {}",b,a,(b%a));
-    let c=2.86;
-    println!("{} nin 3. üssü= {}",c,f64::powi(c,3)); //integer yazmamızın sebebi integer üs almamız.
-    println!("{} nin PI üssü={}",c,f64::powf(c,std::f64::consts::PI));
-    
+
+fn while_and_loop(){
+    let mut i=10;
+    while i<20 {
+        i+=1;
+        if i==12 {continue }
+        println!("{}",i);
+
+
+    }
+
+    let mut y=2;
+    loop {  //while true gibi çalışır
+        y*=2;
+        println!("{}",y);
+        if  y==1<<10 {break}
+    }
+}
+fn main() {
+   // if_state();
+    while_and_loop();
 }
